4cbec0b5054a0a2ca2fda923f3c39ca687689ecf
addyreal <addyreearc@gmail.com>
2026-02-27 20:30:11 +0100

    byte: init

---
+++ byte/export.go
@@ -0,0 +1,44 @@
+package byte
+
+import (
+	"fmt"
+)
+
+type sizeOpts struct {
+	bump  uint64
+	units []string
+}
+
+var (
+	SizeOptBinary *sizeOpts = &sizeOpts{
+		bump:  1024,
+		units: []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"},
+	}
+	SizeOptDecimal *sizeOpts = &sizeOpts{
+		bump:  1000,
+		units: []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"},
+	}
+)
+
+func defaultSizeOpts() *sizeOpts {
+	return SizeOptBinary
+}
+
+func HumanSize(s uint64, o *sizeOpts) string {
+	if o == nil {
+		o = defaultSizeOpts()
+	}
+
+	i := 0
+	if s < o.bump {
+		return fmt.Sprintf("%d%s", s, o.units[i])
+	}
+
+	size := float64(s)
+	for size >= float64(o.bump) {
+		size = size / float64(o.bump)
+		i++
+	}
+
+	return fmt.Sprintf("%.2f%s", size, o.units[i])
+}
---
+++ byte/go.mod
@@ -0,0 +1,3 @@
+module github.com/addyreal/goutils/byte
+
+go 1.25.0
